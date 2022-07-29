0a600b4a9d51699ab6b538c4d0b1bd4b6986247a
Dipan-25 <dipan.codearts@gmail.com>
2022-07-29 17:37:04 +0530

    update Feature Section

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+jQuery('#custom-owl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+jQuery('#custom-feature-owl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+jQuery(document).ready(function(){
+    jQuery('.custom-navbar-toggle').click(function(){
+        jQuery(this).toggleClass('toggle-wrap')
+    })
+});
