0a38e40a521122806faad8760e7884ae5c5594a7
Swagata-codearts <swagata.codearts@gmail.com>
2022-08-01 17:00:48 +0530

    Header toggler

--- js/custom.js
+++ js/custom.js
@@ -36,4 +36,8 @@ jQuery(document).ready(function(){
     jQuery('.custom-navbar-toggle').click(function(){
         jQuery(this).toggleClass('toggle-wrap')
     })
+    jQuery('.custom-carousel-indicator li::marker').attr("--banner-no",'01')
 });
+
+let y   = 10;
+document.documentElement.style.setProperty('--banner-no', y )
